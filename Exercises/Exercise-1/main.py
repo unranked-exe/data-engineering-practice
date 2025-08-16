330de26f67217a7480eab265fc282cf5e42d74e9
unranked-exe <120649405+unranked-exe@users.noreply.github.com>
2025-08-16 23:20:34 +0100

    Completed Ex1 (Syncronous-Forloop)

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,20 +1,50 @@
+import os
 import requests
+import zipfile
+import io
+from shutil import rmtree
 
+# Remove the downloads directory
+rmtree('downloads')
+
+domain = "https://divvy-tripdata.s3.amazonaws.com/{file}"
 download_uris = [
-    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",
-    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q1.zip",
-    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q2.zip",
-    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q3.zip",
-    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q4.zip",
-    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2020_Q1.zip",
-    "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2220_Q1.zip",
+    domain.format(file="Divvy_Trips_2018_Q4.zip"),
+    domain.format(file="Divvy_Trips_2019_Q1.zip"),
+    domain.format(file="Divvy_Trips_2019_Q2.zip"),
+    domain.format(file="Divvy_Trips_2019_Q3.zip"),
+    domain.format(file="Divvy_Trips_2019_Q4.zip"),
+    domain.format(file="Divvy_Trips_2020_Q1.zip"),
+    domain.format(file="Divvy_Trips_2220_Q1.zip"),
 ]
 
+dl_folder = 'downloads'
 
 def main():
     # your code here
-    pass
+    try:
+        os.mkdir(dl_folder)
+        print(f"Directory '{dl_folder}' created successfully.")
+    except FileExistsError:
+        print(f"Directory '{dl_folder}' already exists.")
 
+    try:
+        for link in download_uris:
+            response = requests.get(link)
+            if response.status_code == 200:
+                #Get file name
+                file_name = link.split("/")[-1].replace('.zip', '')
+
+                #Create a BytesIO object
+                zip_file = io.BytesIO(response.content)
+
+                #Extract ZIP to CSV
+                with zipfile.ZipFile(zip_file) as z:
+                    z.extract(file_name + '.csv', dl_folder + '/' + file_name)
+            else:
+                print(f"Failed to download {link}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
     main()
